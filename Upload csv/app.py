743fdc805640d32dc486a3e13fbc8b94d3a2a23e
PRIYADHARSHINI-25 <priya35471@gmail.com>
2022-03-10 22:21:20 +0530

    flask

---
+++ Upload csv/app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,request,flash
+import os
+import sys
+app=Flask(__name__)
+app.secret_key="321"
+
+app.config['UPLOAD_FOLDER1']="Upload csv\static\csv"
+
+@app.route("/",methods=['GET','POST'])
+
+def upload():
+    global csv
+    if request.method=='POST':
+     csv =request.files['upload_csv']
+         
+     if csv.filename!='': 
+    
+      filepath=os.path.join(app.config["UPLOAD_FOLDER1"],csv.filename)
+      csv.save(filepath)
+    
+      flash("File Upload Successfully","success")
+
+    return render_template("upload.html")
+
+if __name__ =='__main__':
+    app.run(debug=True)
